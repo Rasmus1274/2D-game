ca871e0f67af0cda10ce73a613f43376c0c5d85d
Arkudaa <71982604+Arkudaa@users.noreply.github.com>
2021-04-11 13:39:45 +0300

    Versioon0,01

---
+++ 2D game/Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void FixedUpdate()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(xInput*moveSpeed, rb.velocity.y);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
